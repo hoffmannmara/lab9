020bdeb0ac504e50613910c321e69ca551a6e817
hoffmannmara <hoffmannmara@ufl.edu>
2024-04-10 17:21:32 -0400

    first commit

--- lab9mara.py
+++ lab9mara.py
@@ -1,5 +1,18 @@
 def main():
-    def decode():
+    while True:
+        print("Menu\n-------------"
+              "1. Encode"
+              "2. Decode"
+              "3. Quit")
+
+        option = int(input("Please enter an option: "))
+        password = int(input("Please enter your password to encode: "))
+        print("Your password has been encoded and stored!")
+
+        def encode(password):
+
+
+
 
 
 
